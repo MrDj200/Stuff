e857898ed96210d4d6c11767fb35851c4695360b
MrDj200 <mrdj201@gmail.com>
2020-06-20 12:27:31 +0200

    Used prettier on all files

--- chem/scripts/scripts.js
+++ chem/scripts/scripts.js
@@ -1,64 +1,75 @@
 var result = null;
 
-window.onload = () => {      
-    makeRequest();
-    filter();
-}
+window.onload = () => {
+  makeRequest();
+  filter();
+};
 
-function recipeContains(recipe, filter, useChemNames, useRecipeNames)
-{    
-    var returner = false;
-    if (useChemNames) {
-        if (recipe.id == null) {
-            console.log(`Having a problem with ${JSON.stringify(recipe)}\n(id is null)`);
-        }
-        returner = recipe.name == null ? recipe.id?.toLowerCase().includes(filter) : recipe.name.toLowerCase().includes(filter)
+function recipeContains(recipe, filter, useChemNames, useRecipeNames) {
+  var returner = false;
+  if (useChemNames) {
+    if (recipe.id == null) {
+      console.log(
+        `Having a problem with ${JSON.stringify(recipe)}\n(id is null)`
+      );
     }
-    if (useRecipeNames) {
-        returner = returner || recipe.custom_recipe_string.toLowerCase().includes(filter);
-    }
-    return returner;
+    returner =
+      recipe.name == null
+        ? recipe.id?.toLowerCase().includes(filter)
+        : recipe.name.toLowerCase().includes(filter);
+  }
+  if (useRecipeNames) {
+    returner =
+      returner || recipe.custom_recipe_string.toLowerCase().includes(filter);
+  }
+  return returner;
 }
 
 function filter() {
-    let table = document.getElementById("recipes");
-    let filter = document.getElementById("filter").value;
+  let table = document.getElementById("recipes");
+  let filter = document.getElementById("filter").value;
 
-    table.innerHTML = "";
+  table.innerHTML = "";
 
-    if(result == null){
-        return;
+  if (result == null) {
+    return;
+  }
+
+  var useChemNames = document.getElementById("box_chem").checked;
+  var useRecipeNames = document.getElementById("box_recipe").checked;
+
+  var myKeys = Object.keys(result);
+
+  myKeys.sort((a, b) => {
+    if (result[a].name < result[b].name) {
+      return -1;
+    }
+    if (result[a].name > result[b].name) {
+      return 1;
+    }
+    return 0;
+  });
+
+  myKeys.forEach((key) => {
+    var curRecipe = result[key];
+    if (
+      filter != "" &&
+      !recipeContains(curRecipe, filter, useChemNames, useRecipeNames)
+    ) {
+      return;
     }
 
-    var useChemNames = document.getElementById("box_chem").checked;
-    var useRecipeNames = document.getElementById("box_recipe").checked;
-
-    var myKeys = Object.keys(result);
-
-    myKeys.sort((a, b) => {
-        if (result[a].name < result[b].name) {
-            return -1;
-        }
-        if (result[a].name > result[b].name) {
-            return 1;
-        }
-        return 0;
-    });
-
-    myKeys.forEach(key => {
-        var curRecipe = result[key]
-        if (filter != "" && !recipeContains(curRecipe, filter, useChemNames, useRecipeNames)) {
-            return
-        }
-
-        let row = document.createElement("tr");
-        let arr = [curRecipe.name, curRecipe.custom_recipe_string, curRecipe.result_amount];
-        for (let a of arr){
-            let cell = document.createElement("td");
-            cell.innerText = a;
-            row.append(cell);
-        }
-        table.appendChild(row);
-    });
+    let row = document.createElement("tr");
+    let arr = [
+      curRecipe.name,
+      curRecipe.custom_recipe_string,
+      curRecipe.result_amount,
+    ];
+    for (let a of arr) {
+      let cell = document.createElement("td");
+      cell.innerText = a;
+      row.append(cell);
+    }
+    table.appendChild(row);
+  });
 }
-
